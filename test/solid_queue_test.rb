e78f8d986d5f92a5b3f77ee4c5c52e85c2c1aa0b
KingStar365 <KingStar365.21@gmail.com>
2025-04-03 17:50:18 +0200

    Update solid_queue_test.rb

--- test/solid_queue_test.rb
+++ test/solid_queue_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class SolidQueueTest < ActiveSupport::TestCase
-  test "it has a version number" do
+  test " it has a version number " do
     assert SolidQueue::VERSION
   end
 end
